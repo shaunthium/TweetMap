66e210ed98524b21ce4ff6b9a70baaaea84672d6
Shaun Thium <shaun.thium@gmail.com>
2016-08-22 20:11:21 -0700

    Implement toggling of map opacity based on form submission

--- app/frontend/javascripts/input/textbox.js
+++ app/frontend/javascripts/input/textbox.js
@@ -3,15 +3,36 @@ import React from 'react';
 class Textbox extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {text: ''};
+  }
+
+  _onChange = (e) => {
+    this.setState({text: e.target.value});
+  }
+
+  _onSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    // TODO: Submit logic here
+
+    // Change transparency
+    this.props.toggleTransparency();
   }
 
   render() {
     return (
-      <div>
-        <input type="text"/>
+      <div style={{position: 'absolute', left: '45%', top: '30%', zIndex: '999', opacity: '1'}}>
+        <form onSubmit={this._onSubmit}>
+          <input type="text" onChange={this._onChange}/>
+          <button type='submit'>Submit</button>
+        </form>
       </div>
     );
   }
 }
+
+Textbox.propTypes = {
+  toggleTransparency: React.PropTypes.func
+};
 
 export default Textbox;
--- app/frontend/javascripts/misc/map.js
+++ app/frontend/javascripts/misc/map.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import styles from './map.css';
+import {GoogleMapLoader, GoogleMap, Marker} from 'react-google-maps';
+import Textbox from '../input/textbox.js';
 
 class Map extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {transparent: true};
+  }
+
+  _toggleTransparency() {
+    this.setState({transparent: !this.state.transparent});
   }
 
   render() {
     return (
-      <div id='map'></div>
+      <section style={{
+        height: '500px',
+        zIndex: '-999',
+        opacity: (this.state.transparent ? '0.5' : '1')
+      }}>
+        <Textbox
+          toggleTransparency={this._toggleTransparency.bind(this)}/>
+        <GoogleMapLoader
+          containerElement={
+            <div
+              style={{
+                height: "100%",
+              }}
+            />
+          }
+          googleMapElement={
+            <GoogleMap
+              defaultZoom={3}
+              defaultCenter={{ lat: 37.0902, lng: -95.7129 }}
+              onClick={() => false}
+            >
+            </GoogleMap>
+          }
+        />
+      </section>
     );
   }
 }
