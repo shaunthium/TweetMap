d3483c945e508b08190b79bdef4f8cdb697c0518
Shaun Thium <shaun.thium@gmail.com>
2016-08-22 19:12:04 -0700

    Create basic google maps skeleton

--- app/frontend/javascripts/main/main.js
+++ app/frontend/javascripts/main/main.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import styles from './main.css';
-import InputTextbox from '../input/textbox.js';
+import Map from '../misc/map.js';
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
   render() {
     return (
-      <div className={`jumbotron`}>
-        <InputTextbox />
+      <div>
+        <Map />
       </div>
     );
   }
---
+++ app/frontend/javascripts/misc/map.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './map.css';
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id='map'></div>
+    );
+  }
+}
+
+export default Map;
